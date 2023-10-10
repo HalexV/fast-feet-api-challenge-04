de82095534c6230bcf747473a63cb42989391eab
Halex Viotto Gomes <halexx002@gmail.com>
2023-10-10 14:39:08 -0400

    feat: create authenticate admin use case

---
+++ src/domain/fast-feet/application/use-cases/authenticate-admin.ts
@@ -0,0 +1,53 @@
+import { Either, left, right } from '@/core/types/either'
+import { AdminsRepository } from '../repositories/admins-repository'
+import { HashComparer } from '../cryptography/hash-comparer'
+import { Encrypter } from '../cryptography/encrypter'
+import { WrongCredentialsError } from './errors/wrong-credentials-error'
+
+interface AuthenticateAdminUseCaseRequest {
+  cpf: string
+  password: string
+}
+
+type AuthenticateAdminUseCaseResponse = Either<
+  WrongCredentialsError,
+  {
+    accessToken: string
+  }
+>
+
+export class AuthenticateAdminUseCase {
+  constructor(
+    private readonly adminsRepository: AdminsRepository,
+    private readonly hashComparer: HashComparer,
+    private readonly encrypter: Encrypter,
+  ) {}
+
+  async execute({
+    cpf,
+    password,
+  }: AuthenticateAdminUseCaseRequest): Promise<AuthenticateAdminUseCaseResponse> {
+    const admin = await this.adminsRepository.findByCPF(cpf)
+
+    if (!admin) {
+      return left(new WrongCredentialsError())
+    }
+
+    const isPasswordValid = await this.hashComparer.compare(
+      password,
+      admin.password,
+    )
+
+    if (!isPasswordValid) {
+      return left(new WrongCredentialsError())
+    }
+
+    const accessToken = await this.encrypter.encrypt({
+      sub: admin.id.toString(),
+    })
+
+    return right({
+      accessToken,
+    })
+  }
+}
