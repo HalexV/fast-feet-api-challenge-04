51f34e6f1d269f980544273ea0fd4d160ab04d9c
Halex Viotto Gomes <halexx002@gmail.com>
2023-10-10 13:39:39 -0400

    test: should be able to hash admin password upon registration

--- src/domain/fast-feet/application/use-cases/register-admin.spec.ts
+++ src/domain/fast-feet/application/use-cases/register-admin.spec.ts
@@ -30,4 +30,23 @@ describe('Register Admin', () => {
       expect(inMemoryAdminsRepository.items[0]).toEqual(result.value.admin)
     }
   })
+
+  it('should be able to hash admin password upon registration', async () => {
+    const result = await sut.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      cpf: '11122233304',
+      password: '12345678',
+      address: 'john doe street, 450',
+      district: 'center',
+      city: 'example city',
+      state: 'RO',
+    })
+
+    const hashedPassword = await fakeHasher.hash('12345678')
+
+    expect(result.isRight()).toBeTruthy()
+
+    expect(inMemoryAdminsRepository.items[0].password).toEqual(hashedPassword)
+  })
 })
