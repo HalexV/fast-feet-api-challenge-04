108625ee03845f86202520d4764019e87c947a5e
Halex Viotto Gomes <halexx002@gmail.com>
2023-10-13 14:59:15 -0400

    test: it should not be able to get a delivery person that does not exist

--- src/domain/fast-feet/application/use-cases/get-delivery-person.spec.ts
+++ src/domain/fast-feet/application/use-cases/get-delivery-person.spec.ts
@@ -1,6 +1,7 @@
 import { InMemoryDeliveryPeopleRepository } from 'test/repositories/in-memory-delivery-people-repository'
 import { makeDeliveryPerson } from 'test/factories/make-delivery-person'
 import { GetDeliveryPersonUseCase } from './get-delivery-person'
+import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
 
 let inMemoryDeliveryPeopleRepository: InMemoryDeliveryPeopleRepository
 let sut: GetDeliveryPersonUseCase
@@ -27,6 +28,17 @@ describe('Get delivery person', () => {
       expect(result.value.deliveryPerson).toMatchObject({
         name: 'John Doe',
       })
+    }
+  })
+
+  it('should not be able to get a delivery person that does not exist', async () => {
+    const result = await sut.execute({
+      id: 'non-existent-id',
+    })
+
+    expect(result.isLeft()).toBeTruthy()
+    if (result.isLeft()) {
+      expect(result.value).toBeInstanceOf(ResourceNotFoundError)
     }
   })
 })
