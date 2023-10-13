d64b640fd72bd1cd5eac39ef4151cd896d1a0504
Halex Viotto Gomes <halexx002@gmail.com>
2023-10-13 16:47:00 -0400

    test: it should not be able to register a recipient with same email

--- src/domain/fast-feet/application/use-cases/register-recipient.spec.ts
+++ src/domain/fast-feet/application/use-cases/register-recipient.spec.ts
@@ -1,5 +1,6 @@
 import { InMemoryRecipientsRepository } from 'test/repositories/in-memory-recipients-repository'
 import { RegisterRecipientUseCase } from './register-recipient'
+import { RecipientAlreadyExistsError } from './errors/recipient-already-exists-error'
 
 let inMemoryRecipientsRepository: InMemoryRecipientsRepository
 let sut: RegisterRecipientUseCase
@@ -26,6 +27,33 @@ describe('Register recipient', () => {
       expect(inMemoryRecipientsRepository.items[0]).toEqual(
         result.value.recipient,
       )
+    }
+  })
+
+  it('should not be able to register a recipient with same email', async () => {
+    await sut.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      address: 'john doe street, 450',
+      district: 'center',
+      city: 'example city',
+      state: 'RO',
+      zipcode: '11222000',
+    })
+
+    const result = await sut.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      address: 'john doe street, 450',
+      district: 'center',
+      city: 'example city',
+      state: 'RO',
+      zipcode: '11222000',
+    })
+
+    expect(result.isLeft()).toBeTruthy()
+    if (result.isLeft()) {
+      expect(result.value).toBeInstanceOf(RecipientAlreadyExistsError)
     }
   })
 })
