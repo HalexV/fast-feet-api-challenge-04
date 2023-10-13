56f43724177f1d84c4193fcfede841efbae23585
Halex Viotto Gomes <halexx002@gmail.com>
2023-10-13 16:12:59 -0400

    feat: create authenticate delivery person use case

---
+++ src/domain/fast-feet/application/use-cases/authenticate-delivery-person.ts
@@ -0,0 +1,53 @@
+import { Either, left, right } from '@/core/types/either'
+import { HashComparer } from '../cryptography/hash-comparer'
+import { Encrypter } from '../cryptography/encrypter'
+import { WrongCredentialsError } from './errors/wrong-credentials-error'
+import { DeliveryPeopleRepository } from '../repositories/delivery-people-repository'
+
+interface AuthenticateDeliveryPersonUseCaseRequest {
+  cpf: string
+  password: string
+}
+
+type AuthenticateDeliveryPersonUseCaseResponse = Either<
+  WrongCredentialsError,
+  {
+    accessToken: string
+  }
+>
+
+export class AuthenticateDeliveryPersonUseCase {
+  constructor(
+    private readonly deliveryPeopleRepository: DeliveryPeopleRepository,
+    private readonly hashComparer: HashComparer,
+    private readonly encrypter: Encrypter,
+  ) {}
+
+  async execute({
+    cpf,
+    password,
+  }: AuthenticateDeliveryPersonUseCaseRequest): Promise<AuthenticateDeliveryPersonUseCaseResponse> {
+    const deliveryPerson = await this.deliveryPeopleRepository.findByCPF(cpf)
+
+    if (!deliveryPerson) {
+      return left(new WrongCredentialsError())
+    }
+
+    const isPasswordValid = await this.hashComparer.compare(
+      password,
+      deliveryPerson.password,
+    )
+
+    if (!isPasswordValid) {
+      return left(new WrongCredentialsError())
+    }
+
+    const accessToken = await this.encrypter.encrypt({
+      sub: deliveryPerson.id.toString(),
+    })
+
+    return right({
+      accessToken,
+    })
+  }
+}
