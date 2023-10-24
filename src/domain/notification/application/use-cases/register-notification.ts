3a7e49509b231d86655a1a8eb4d4eb6c93b950a7
Halex Viotto Gomes <halexx002@gmail.com>
2023-10-24 13:45:21 -0400

    feat: create register notification use case

---
+++ src/domain/notification/application/use-cases/register-notification.ts
@@ -0,0 +1,41 @@
+import { Either, right } from '@/core/types/either'
+import { Notification } from '../../enterprise/entities/Notification'
+import { NotificationsRepository } from '../repositories/notifications-repository'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+
+interface RegisterNotificationUseCaseRequest {
+  title: string
+  content: string
+  recipientId: string
+}
+
+type RegisterNotificationUseCaseResponse = Either<
+  null,
+  {
+    notification: Notification
+  }
+>
+
+export class RegisterNotificationUseCase {
+  constructor(
+    private readonly notificationsRepository: NotificationsRepository,
+  ) {}
+
+  async execute({
+    title,
+    content,
+    recipientId,
+  }: RegisterNotificationUseCaseRequest): Promise<RegisterNotificationUseCaseResponse> {
+    const notification = Notification.create({
+      title,
+      content,
+      recipientId: new UniqueEntityId(recipientId),
+    })
+
+    await this.notificationsRepository.create(notification)
+
+    return right({
+      notification,
+    })
+  }
+}
