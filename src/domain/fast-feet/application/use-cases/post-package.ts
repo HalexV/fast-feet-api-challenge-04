4e3ad0afc745b4fb3f7903094028fac9ec66af27
Halex Viotto Gomes <halexx002@gmail.com>
2023-11-01 14:09:22 -0400

    feat: create post package use case

---
+++ src/domain/fast-feet/application/use-cases/post-package.ts
@@ -0,0 +1,47 @@
+import { Either, left, right } from '@/core/types/either'
+import { Package } from '../../enterprise/entities/Package'
+import { PackagesRepository } from '../repositories/packages-repository'
+import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
+import { RecipientsRepository } from '../repositories/recipients-repository'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+
+interface PostPackageUseCaseRequest {
+  description: string
+  recipientId: string
+}
+
+type PostPackageUseCaseResponse = Either<
+  ResourceNotFoundError,
+  {
+    pkg: Package
+  }
+>
+
+export class PostPackageUseCase {
+  constructor(
+    private readonly packagesRepository: PackagesRepository,
+    private readonly recipientsRepository: RecipientsRepository,
+  ) {}
+
+  async execute({
+    description,
+    recipientId,
+  }: PostPackageUseCaseRequest): Promise<PostPackageUseCaseResponse> {
+    const recipient = await this.recipientsRepository.findById(recipientId)
+
+    if (!recipient) {
+      return left(new ResourceNotFoundError())
+    }
+
+    const pkg = Package.create({
+      description,
+      recipientId: new UniqueEntityId(recipientId),
+    })
+
+    await this.packagesRepository.create(pkg)
+
+    return right({
+      pkg,
+    })
+  }
+}
