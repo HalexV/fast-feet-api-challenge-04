ff7a0243ebb98eb3a55d34c0eb43c30b68bd993a
Halex Viotto Gomes <halexx002@gmail.com>
2023-11-02 16:37:09 -0400

    test: it should not be able to edit a package with a non-existent status

--- src/domain/fast-feet/application/use-cases/edit-package.spec.ts
+++ src/domain/fast-feet/application/use-cases/edit-package.spec.ts
@@ -8,6 +8,8 @@ import { InMemoryDeliveryPeopleRepository } from 'test/repositories/in-memory-de
 import { makePackage } from 'test/factories/make-package'
 import { makeDeliveryPerson } from 'test/factories/make-delivery-person'
 import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
+import { Status } from '@/core/types/status'
+import { PackageStatusNotAllowedError } from './errors/package-status-not-allowed-error'
 
 let inMemoryPhotosRepository: InMemoryPhotosRepository
 let inMemoryPackagesRepository: InMemoryPackagesRepository
@@ -154,6 +156,36 @@ describe('Edit package', () => {
     expect(result.isLeft()).toBeTruthy()
     if (result.isLeft()) {
       expect(result.value).toBeInstanceOf(ResourceNotFoundError)
+    }
+  })
+
+  it('should not be able to edit a package with a non-existent status', async () => {
+    const pkg = makePackage()
+    const recipient = makeRecipient()
+    const deliveryPerson = makeDeliveryPerson()
+
+    inMemoryPackagesRepository.create(pkg)
+    inMemoryRecipientsRepository.create(recipient)
+    inMemoryDeliveryPeopleRepository.create(deliveryPerson)
+
+    const postedAtDate = new Date('2023-01-01')
+    const withdrewDate = new Date('2023-01-01')
+    const deliveredAtDate = new Date('2023-01-07')
+
+    const result = await sut.execute({
+      id: pkg.id.toString(),
+      description: 'Description edited',
+      postedAt: postedAtDate,
+      recipientId: recipient.id.toString(),
+      status: 'not-existent' as Status,
+      withdrewAt: withdrewDate,
+      deliveredAt: deliveredAtDate,
+      deliveryPersonId: deliveryPerson.id.toString(),
+    })
+
+    expect(result.isLeft()).toBeTruthy()
+    if (result.isLeft()) {
+      expect(result.value).toBeInstanceOf(PackageStatusNotAllowedError)
     }
   })
 })
