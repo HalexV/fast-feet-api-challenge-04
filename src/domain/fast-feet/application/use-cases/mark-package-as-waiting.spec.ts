02c10b7d38dce70ac4437d11d10f7bf4a620ecba
Halex Viotto Gomes <halexx002@gmail.com>
2023-11-03 13:44:58 -0400

    test: it should be able to mark a package as waiting

---
+++ src/domain/fast-feet/application/use-cases/mark-package-as-waiting.spec.ts
@@ -0,0 +1,47 @@
+import { InMemoryPackagesRepository } from 'test/repositories/in-memory-packages-repository'
+import { InMemoryPhotosRepository } from 'test/repositories/in-memory-photos-repository'
+import { makePackage } from 'test/factories/make-package'
+import { MarkPackageAsWaitingUseCase } from './mark-package-as-waiting'
+
+let inMemoryPhotosRepository: InMemoryPhotosRepository
+let inMemoryPackagesRepository: InMemoryPackagesRepository
+let sut: MarkPackageAsWaitingUseCase
+
+describe('Mark package as waiting', () => {
+  beforeEach(() => {
+    inMemoryPhotosRepository = new InMemoryPhotosRepository()
+    inMemoryPackagesRepository = new InMemoryPackagesRepository(
+      inMemoryPhotosRepository,
+    )
+
+    sut = new MarkPackageAsWaitingUseCase(inMemoryPackagesRepository)
+  })
+
+  it('should be able to mark a package as waiting', async () => {
+    const pkg = makePackage()
+    const pkg2 = makePackage({
+      status: 'returned',
+    })
+
+    await inMemoryPackagesRepository.create(pkg)
+    await inMemoryPackagesRepository.create(pkg2)
+
+    const result = await sut.execute({
+      id: pkg.id.toString(),
+    })
+
+    const result2 = await sut.execute({
+      id: pkg2.id.toString(),
+    })
+
+    expect(result.isRight()).toBeTruthy()
+    if (result.isRight()) {
+      expect(inMemoryPackagesRepository.items[0].status).toBe('waiting')
+    }
+
+    expect(result2.isRight()).toBeTruthy()
+    if (result2.isRight()) {
+      expect(inMemoryPackagesRepository.items[1].status).toBe('waiting')
+    }
+  })
+})
