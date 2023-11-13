f7bb5ace73b14b6bf8ae7ac1d4acbab664b8dc45
Halex Viotto Gomes <halexx002@gmail.com>
2023-11-13 16:15:01 -0400

    test: it should be able to turn an admin into a delivery person

---
+++ src/domain/fast-feet/application/use-cases/turn-admin-into-delivery-person.spec.ts
@@ -0,0 +1,46 @@
+import { InMemoryDeliveryPeopleRepository } from 'test/repositories/in-memory-delivery-people-repository'
+import { TurnAdminIntoDeliveryPersonUseCase } from './turn-admin-into-delivery-person'
+import { InMemoryAdminsRepository } from 'test/repositories/in-memory-admins-repository'
+import { makeAdmin } from 'test/factories/make-admin'
+
+let inMemoryDeliveryPeopleRepository: InMemoryDeliveryPeopleRepository
+let inMemoryAdminsRepository: InMemoryAdminsRepository
+let sut: TurnAdminIntoDeliveryPersonUseCase
+
+describe('Turn admin into delivery person', () => {
+  beforeEach(() => {
+    inMemoryAdminsRepository = new InMemoryAdminsRepository()
+    inMemoryDeliveryPeopleRepository = new InMemoryDeliveryPeopleRepository()
+    sut = new TurnAdminIntoDeliveryPersonUseCase(
+      inMemoryAdminsRepository,
+      inMemoryDeliveryPeopleRepository,
+    )
+  })
+
+  it('should be able to turn an admin into a delivery person', async () => {
+    const admin = makeAdmin()
+
+    await inMemoryAdminsRepository.create(admin)
+
+    const result = await sut.execute({
+      adminId: admin.id.toString(),
+    })
+
+    expect(result.isRight()).toBeTruthy()
+    if (result.isRight()) {
+      expect(inMemoryDeliveryPeopleRepository.items[0]).toEqual(
+        result.value.deliveryPerson,
+      )
+      expect(inMemoryDeliveryPeopleRepository.items[0]).toMatchObject({
+        address: admin.address,
+        city: admin.city,
+        cpf: admin.cpf,
+        district: admin.district,
+        email: admin.email,
+        name: admin.name,
+        password: admin.password,
+        state: admin.state,
+      })
+    }
+  })
+})
--- test/repositories/in-memory-admins-repository.ts
+++ test/repositories/in-memory-admins-repository.ts
@@ -4,6 +4,16 @@ import { Admin } from '@/domain/fast-feet/enterprise/entities/Admin'
 export class InMemoryAdminsRepository implements AdminsRepository {
   public items: Admin[] = []
 
+  async findById(id: string): Promise<Admin | null> {
+    const admin = this.items.find((admin) => admin.id.toString() === id)
+
+    if (!admin) {
+      return null
+    }
+
+    return admin
+  }
+
   async findByCPF(cpf: string): Promise<Admin | null> {
     const admin = this.items.find((admin) => admin.cpf === cpf)
 
