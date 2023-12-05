f8bcb23f09c835ac3ce6e52857d74622db7a8efe
Halex Viotto Gomes <halexx002@gmail.com>
2023-12-05 17:23:14 -0400

    feat: replace postgres env vars to one env var and create env example

--- src/infra/env/env.ts
+++ src/infra/env/env.ts
@@ -2,11 +2,7 @@ import { z } from 'zod'
 
 export const envSchema = z.object({
   APP_PORT: z.coerce.number().default(3333),
-  POSTGRES_HOST: z.string(),
-  POSTGRES_PORT: z.coerce.number().default(5432),
-  POSTGRES_USER: z.string(),
-  POSTGRES_PASSWORD: z.string(),
-  POSTGRES_DB: z.string(),
+  POSTGRES_URL: z.string(),
 })
 
 export type Env = z.infer<typeof envSchema>
