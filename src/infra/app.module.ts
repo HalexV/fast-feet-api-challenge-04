17b5854ea36f48657e0b238ff22a9f8887e40a17
Halex Viotto Gomes <halexx002@gmail.com>
2023-12-07 19:49:17 -0400

    feat: create default admin on nest start up

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common'
+import { Logger, Module, OnApplicationBootstrap } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { envSchema } from './env/env'
 import { EnvModule } from './env/env.module'
 import { DatabaseModule } from './database/database.module'
 import { EnvService } from './env/env.service'
+import { CryptographyModule } from './cryptography/cryptography.module'
+import { RegisterDefaultAdminUseCase } from '@/domain/fast-feet/application/use-cases/register-default-admin'
 
 @Module({
   imports: [
@@ -19,6 +21,37 @@ import { EnvService } from './env/env.service'
         connectionString: envService.get('POSTGRES_URL'),
       }),
     }),
+    CryptographyModule,
   ],
+  providers: [RegisterDefaultAdminUseCase],
 })
-export class AppModule {}
+export class AppModule implements OnApplicationBootstrap {
+  private readonly logger = new Logger(AppModule.name)
+
+  constructor(
+    private readonly registerDefaultAdmin: RegisterDefaultAdminUseCase,
+    private readonly envService: EnvService,
+  ) {}
+
+  async onApplicationBootstrap() {
+    const result = await this.registerDefaultAdmin.execute({
+      address: 'default',
+      city: 'default',
+      state: 'AC',
+      cpf: '00000000000',
+      district: 'default',
+      email: 'default@default.com',
+      name: 'default',
+      password: this.envService.get('DEFAULT_ADMIN_PASSWORD'),
+    })
+
+    if (result.isRight()) {
+      if (!result.value) {
+        this.logger.log('Default admin already created')
+        return
+      }
+
+      this.logger.log('Default admin created')
+    }
+  }
+}
