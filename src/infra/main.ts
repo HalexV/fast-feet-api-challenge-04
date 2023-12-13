b4613c245b23503b3a2aebd9e5805d2985657045
Halex Viotto Gomes <halexx002@gmail.com>
2023-12-13 14:37:24 -0400

    feat: generate swagger documentation

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { EnvService } from './env/env.service'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const envService = app.get(EnvService)
+
+  const config = new DocumentBuilder()
+    .setTitle('Fast-Feet API')
+    // .setDescription('The cats API description')
+    .setVersion('1.0')
+    // .addTag('cats')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
 
   const port = envService.get('APP_PORT')
 
