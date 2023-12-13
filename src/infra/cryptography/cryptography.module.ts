15e2387a3562881523d73114cf8642fac43dc748
Halex Viotto Gomes <halexx002@gmail.com>
2023-12-13 14:36:17 -0400

    feat: add jwt encrypter to cryptography module

--- src/infra/cryptography/cryptography.module.ts
+++ src/infra/cryptography/cryptography.module.ts
@@ -2,12 +2,15 @@ import { HashGenerator } from '@/domain/fast-feet/application/cryptography/hash-
 import { Module } from '@nestjs/common'
 import { BcryptHasher } from './bcrypt-hasher'
 import { HashComparer } from '@/domain/fast-feet/application/cryptography/hash-comparer'
+import { Encrypter } from '@/domain/fast-feet/application/cryptography/encrypter'
+import { JwtEncrypter } from './jwt-encrypter'
 
 @Module({
   providers: [
     { provide: HashGenerator, useClass: BcryptHasher },
     { provide: HashComparer, useClass: BcryptHasher },
+    { provide: Encrypter, useClass: JwtEncrypter },
   ],
-  exports: [HashGenerator, HashComparer],
+  exports: [HashGenerator, HashComparer, Encrypter],
 })
 export class CryptographyModule {}
